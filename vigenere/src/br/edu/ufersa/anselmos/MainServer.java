c193c429d57ef7bf738340f9b66a8ee8cd35da8d
Ascênio Sanderson <ascenio@hotmail.com>
2022-03-15 23:52:56 -0300

    feat: melhora tratamento de erro e logs do `MainServer`

--- vigenere/src/br/edu/ufersa/anselmos/MainServer.java
+++ vigenere/src/br/edu/ufersa/anselmos/MainServer.java
@@ -7,12 +7,23 @@ import br.edu.ufersa.anselmos.config.ServerConfig;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainServer {
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+    private static final Logger logger = Logger.getLogger(MainServer.class.getName());
+
+    public static void main(String[] args) {
         var cifra = new CifraVigenere(CifraConfig.CHAVE);
-        var registry = LocateRegistry.createRegistry(ServerConfig.PORTA);
-        var server = new ConsoleServerLogger(cifra);
-        registry.bind(ServerConfig.NOME, server);
+        try {
+            var registry = LocateRegistry.createRegistry(ServerConfig.PORTA);
+            var server = new ConsoleServerLogger(cifra);
+            registry.bind(ServerConfig.NOME, server);
+            logger.log(Level.SEVERE, "Started");
+        } catch (RemoteException e) {
+            logger.log(Level.SEVERE, "Não foi possível obter o registry");
+        } catch (AlreadyBoundException e) {
+            logger.log(Level.SEVERE, "Já existe uma instância do servidor cadastrada para '" + ServerConfig.NOME + "'");
+        }
     }
 }
